46eaf349aa9023abaff51fbe52084b97d2cb8a83
hylle03 <mahy23ab@student.cbs.dk>
2024-11-25 06:19:50 +0100

    Fix af database connection

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,12 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+import dotenv from 'dotenv';
+dotenv.config();
+import { config } from './config.js';
+import Database from './database/database.js';
+
+
 
 const apiRoutes = require('./routes/apiRoutes');
 const viewRoutes = require('./routes/viewRoutes');
